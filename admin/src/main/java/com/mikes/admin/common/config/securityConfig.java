60b46a777b94b17c5b1c7945295927828b12b1c1
Codetransform <18204099093@163.com>
2022-04-30 22:54:35 +0800

    更新security访问控制

--- admin/src/main/java/com/mikes/admin/common/config/securityConfig.java
+++ admin/src/main/java/com/mikes/admin/common/config/securityConfig.java
@@ -12,5 +12,17 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/500").permitAll()
+                .antMatchers("/403").permitAll()
+                .antMatchers("/404").permitAll()
+                .anyRequest() //任何其它请求
+                .authenticated() //都需要身份认证
+                .and()
+                .formLogin() //使用表单认证方式
+                .loginProcessingUrl("/login")//配置默认登录入口
+                .and()
+                .csrf().disable();
     }
 }
